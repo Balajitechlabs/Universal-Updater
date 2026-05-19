31a885e7d6c8a8af9880281daae478873a196482
BALAJI S <212744006+Balajitechlabs@users.noreply.github.com>
2026-05-19 16:41:49 +0530

    Fix Prettier code style issues

--- src/export-backups.tsx
+++ src/export-backups.tsx
@@ -61,7 +61,12 @@ export async function createBackup(): Promise<{
       "mas",
       "go",
     ] as EcosystemId[]
-  ).filter((id) => prefs[`enable${id.charAt(0).toUpperCase() + id.slice(1)}` as keyof Preferences]);
+  ).filter(
+    (id) =>
+      prefs[
+        `enable${id.charAt(0).toUpperCase() + id.slice(1)}` as keyof Preferences
+      ],
+  );
 
   const backup: Record<string, Record<string, string>> = {};
   let packageCount = 0;
